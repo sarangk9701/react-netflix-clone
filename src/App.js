76ed6319b326599e3cb3662345ff5bb328331202
valla <valla@vallaui-MacBookPro.local>
2022-12-14 14:19:41 +0900

    slider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import "./App.css";
 import Nav from "./components/Nav";
 import Banner from "./components/Banner";
+import requests from "./api/requests";
+import Row from "./components/Row";
+import Footer from "./components/Footer";
 
 function App() {
   return (
+
     <div className='App'>
       <Nav />
       <Banner />
+
+      <Row
+        title="NETFLIX ORIGINALS"
+        id="NO"
+        fetchUrl = {requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+
+      <Row title="Trending NOW" id="TN" fetchUrl= {requests.fetchTrending}/>
+      <Row title="Top Rated" id="TR" fetchUrl= {requests.fetchTopRated}/>
+      <Row title="Action Movies" id="AM" fetchUrl= {requests.fetchActionMovies}/>
+      <Row title="Comedy Movies" id="CM" fetchUrl= {requests.fetchComedyMovies}/>
+
+      <Footer />
     </div>
   );
 }
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,9 +2,13 @@ import axios from '../api/axios';
 import React, { useEffect, useState } from 'react';
 import requests from '../api/requests';
 import "./Banner.css";
+import styled from 'styled-components';
 
 export default function Banner() {
     const [movie, setMovie] = useState([]);
+
+    const [isClicked, setIsClicked] = useState(false);
+
     useEffect (() => {
         fetchData();
     },[]);
@@ -23,7 +27,7 @@ export default function Banner() {
         //특정 영화의 더 상세한 정보를 가져오기 (비디오 정보도 포함)
         //const results = await axios.get(`movie/${movieId}`, {
         const { data : movieDetail } = await axios.get(`movie/${movieId}`, {
-            params: {append_to_reponse: "video"},
+            params: {append_to_response: "video"},
         });
         //console.log('results',results)
         setMovie(movieDetail);
@@ -32,26 +36,76 @@ export default function Banner() {
     const truncate = (str,n)=>{
         return str?.length > n ? str.substr(0, n-1) + "...": str;
     }
+    console.log(movie)
 
-  return (
-    < header
-        className='banner'
-        style={{
-            background: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
-            backgroundPosition: "top center",
-            backgroundSize: "cover"
-        }}
-    >
-    <div className='banner__contents'>
-        <h1 className='banner__title'>{movie.title || movie.name || movie.original_name}</h1>
-        <div className='banner__buttons'>
-            <button className="banner__button play">Play</button>
-            <button className="banner__button info">More Information</button>
-        </div>
+    if(!isClicked) {
+        return (
+            < header
+                className='banner'
+                style={{
+                    background: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+                    backgroundPosition: "top center",
+                    backgroundSize: "cover"
+                }}
+            >
+            <div className='banner__contents'>
+                <h1 className='banner__title'>{movie.title || movie.name || movie.original_name}</h1>
+                <div className='banner__buttons'>
+                    <button className="banner__button play" onClick={() => setIsClicked(true)}>Play</button>
+                    <button className="banner__button info">More Information</button>
+                </div>
+        
+                <h1 className='banner__description'>{truncate(movie.overview,100)}</h1>
+            </div>
+            <div className='banner--fadeBottom' />
+            </header>
+          );
 
-        <h1 className='banner__description'>{truncate(movie.overview,100)}</h1>
-    </div>
-    <div className='banner--fadeBottom' />
-    </header>
-  )
+    } else {
+        return (
+            <Container>
+                <HomeContainer> 
+                    <Iframe 
+                    width="640" 
+                    height="360" 
+                    src={`https://www.youtube.com/embed/${movie.videos.results[0]?.key}
+                    ?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0]?.key}`}
+                    title="YouTube video player" 
+                    frameborder="0" 
+                    allow="autoplay; fullscreen" 
+                    ></Iframe>
+                </HomeContainer>
+            </Container>
+        )
+    }
+
 }
+const Iframe = styled.iframe`
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    opacity: 0.65;
+    border : none;
+
+    &::after{
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+`;
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    height: 100vh
+`;
+
+const HomeContainer = styled.div`
+    widht: 100%;
+    height: 100%;
+`;
