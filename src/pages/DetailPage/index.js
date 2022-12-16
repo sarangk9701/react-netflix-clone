96c47c592536043a3b956f3ae0b8bb5014693ab3
valla <valla@vallaui-MacBookPro.local>
2022-12-16 09:07:51 +0900

    detail page

--- src/pages/DetailPage/index.js
+++ src/pages/DetailPage/index.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, {useState,useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from '../../api/axios';
 
 export default function DetailPage() {
+
+  const { movieId } = useParams();
+  console.log('movieID', movieId);
+  const [movie, setMovie] = useState({});
+
+  useEffect(()=>{
+    async function fetchData() {
+      const request = await axios.get(
+        `/movie/${movieId}`
+      )
+      console.log('request', request);
+      setMovie(request.data)
+    }
+    fetchData();
+  },[movieId]);
+
+  if( !movie ) return <div>...loading</div>;
   return (
-    <div>index</div>
+    <section>
+      <img
+        className='modal__poster-img'
+        src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+        alt = "poster"
+      />
+      
+    </section>
   )
 }
--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,28 +1,38 @@
 import axios from '../../api/axios';
 import React,{useEffect,useState} from 'react'
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import "./SearchPage.css"
+import { useDebounce } from '../../hooks/useDebounce';
 
 export default function SearchPage() {
   
-  console.log('useLocation()', useLocation());
-
+  //console.log('useLocation()', useLocation());
+  const navigate = useNavigate();
   const [searchResults, setSearchResults] = useState([]);
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  }
+
 
   let query = useQuery();
   const searchTerm = query.get("q");
-  console.log('searchTerm: ', searchTerm);
+  //console.log('searchTerm: ', searchTerm);
+
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
+  console.log('debouncedSearchTerm: ', debouncedSearchTerm);
 
   useEffect(() => {
-    if(searchTerm) {
-      fetchSearchMovie(searchTerm);
+    if(debouncedSearchTerm) {
+      fetchSearchMovie(debouncedSearchTerm);
     }
-  }, [searchTerm]);
+  }, [debouncedSearchTerm]);
 
-  const fetchSearchMovie = async (searchTerm) => {
+  const fetchSearchMovie = async (debouncedSearchTerm) => {
+    //console.log('searchTerm : ', searchTerm);
     try{
       const request = await axios.get(
-        `/search/multi?include_adult=false&query=${searchTerm}`
-      )
+        `/search/multi?include_adult=false&query=${debouncedSearchTerm}`
+      );
       console.log(request);
       setSearchResults(request.data.results);
     }catch(error){
@@ -30,11 +40,37 @@ export default function SearchPage() {
     }
   };
 
-  const useQuery = () => {
-    return new URLSearchParams(useLocation().search);
+  const renderSearchResults = () => {
+    return searchResults.length > 0 ? (
+      <section className='search-container'>
+        {searchResults.map((movie) => {
+          if(movie.backdrop_path != null && movie.media_type !== "person"){
+            const movieImageUrl = 
+              "https://image.tmdb.org/t/p/w500" + movie.backdrop_path
+              return (
+                <div className='movie' key={movie.id}>
+                  <div onClick={() => navigate(`/${movie.id}`)}
+                  className='movie__colum-poster'>
+                      <img
+                        src={movieImageUrl} alt="movie" 
+                        className='movie__poster'
+                      />
+                  </div>
+                </div>
+              )
+          }
+        })}
+      </section>
+    ) : (
+      <section className='no-results'>
+        <div className='no-results__text'>
+          <p>
+            찾고자 하는 검색어 "{debouncedSearchTerm}"에 맞는 영화가 없습니다.
+          </p>
+        </div>
+      </section>
+    )
   }
 
-  return (
-    <div>SearchPage</div>
-  )
+  return renderSearchResults();
 }
